be765ab16b265c02763aa745840b8a82c679c2de
Ian Fox <ianfox41@gmail.com>
2016-02-09 07:21:16 +0000

    Ready for Heroku!

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,9 +7,66 @@ class HangpersonGame
 
   # def initialize()
   # end
+  attr_accessor :word, :guesses, :wrong_guesses, :word_with_guesses
   
   def initialize(word)
     @word = word
+    @guesses = ""
+    @wrong_guesses = ""
+    @word_with_guesses = "-" * self.word.length
+  end
+  
+  def append_wrong_guess(character)
+    if self.wrong_guesses().include?(character) == false
+      self.wrong_guesses= (self.wrong_guesses + character)
+      return true
+    else
+      return false
+    end
+  end
+  
+  
+  def fill_character(character)
+    for pos in 0..self.word_with_guesses.length - 1
+        if character == self.word[pos]
+          self.word_with_guesses[pos] = character
+        end
+    end
+  end
+
+  def append_guess(character)
+    if self.guesses().include?(character) == false
+      self.guesses= (self.guesses + character)
+      self.fill_character(character)
+      return true
+    else
+      return false
+    end
+  end
+   
+  def guess (character)
+    if (character != nil and character.length == 1 and  \
+          (character.downcase == character.downcase[/[a-z]/]))
+      character = character.downcase
+      if self.word.include? character
+        return self.append_guess(character)
+      else
+        return self.append_wrong_guess(character)
+      end
+    else
+      return raise ArgumentError.new
+    end
+  end
+  
+  def check_win_or_lose
+    if self.guesses.length == self.word.length
+      return :win
+    elsif self.wrong_guesses.length >= 7
+      return :lose
+    else
+      return :play
+    end
+      
   end
 
   def self.get_random_word
